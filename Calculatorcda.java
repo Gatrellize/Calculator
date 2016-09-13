5e506e2200791522aec53434de6f21ae7134f8c2
Leo G <lgatrell@utcmediacityuk.org.uk>
2016-09-13 12:31:37 +0100

    Add files via upload

---
+++ Calculatorcda.java
@@ -0,0 +1,19 @@
+
+package calculatorcda;
+
+public class Calculatorcda {
+
+    public static void main(String[] args) {
+
+        String[] ArrayList; 
+        ArrayList = new String[5];
+
+        ArrayList[0] = "Alice"; 
+        ArrayList[1] = "Bob"; 
+        ArrayList[2] = "Claire"; 
+        ArrayList[3] = "Dan"; 
+        ArrayList[4] = "Eve";
+        
+    }
+    
+}
